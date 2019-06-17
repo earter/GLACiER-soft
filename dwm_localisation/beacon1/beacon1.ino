96a0d373c29437dddc9b5407facc133f6ecdc2dd
Julia W <jw@jw.com>
2019-06-17 11:10:55 +0200

    dwm folder update

---
+++ dwm_localisation/beacon1/beacon1.ino
@@ -0,0 +1,44 @@
+#include <SPI.h>
+#include "DW1000Ranging.h"
+#include "DW1000Device.h"
+
+// MICRO connection pins
+
+const uint8_t PIN_RST = 3; // reset pin
+const uint8_t PIN_IRQ = 7; // irq pin
+const uint8_t PIN_SS = 10; // spi select pin
+/*
+// MEGA connection pins
+const uint8_t PIN_RST = 46; // reset pin
+const uint8_t PIN_IRQ = 48; // irq pin
+const uint8_t PIN_SS = 53; // spi select pin
+*/
+/* MISO = 50  MOSI = 51  SCK = 52  */
+
+void setup() {
+  // Serial is for communication between the computer and the board.
+  Serial.begin(9600);
+
+  delay(1000);
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ);
+  DW1000Ranging.attachNewRange(newRange);
+  DW1000Ranging.startAsAnchor("2A:1A:1A:1A:1A:1A:1A:1A", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+}
+
+void loop() {
+  DW1000Ranging.loop();
+
+  Serial.print(DW1000Ranging.getTagX());
+  Serial.print(" ");
+  Serial.println(DW1000Ranging.getTagY());
+  if(DW1000Ranging.getTagY() > 4.0f){
+    digitalWrite(6,HIGH);
+  }
+  else digitalWrite(6,LOW);
+}
+
+void newRange() {
+  // Empty.  
+}
+
+// 23 26 3
