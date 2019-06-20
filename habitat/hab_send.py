18a1bc50e42693c5eebe69c9bf89ced5c6a8688f
Julia W <jw@jw.com>
2019-06-20 11:08:00 +0200

    added habitat

--- habitat/hab_send.py
+++ habitat/hab_send.py
@@ -4,12 +4,16 @@ port_name = "/dev/ttyACM0"
 ser_port = serial.Serial(port_name, 115200)
 print(f"port opened: {ser_port.name}")
 
+payload = "co jjjjjtam?"
 
-data = "<FROM=HAB#TO=AS#TEXT=TOMASZMIS#>.........................................................................................................................................................................................................................."
+frame = f"<FROM=HAB#TO=AS#TEXT={payload}#>"
+
+for i in range(250-len(frame)):
+    frame += "."
 
 def send_serial(ser, data):
             ser.write(str.encode(data))
             print(str.encode(data), f"port: {ser}")
             #time.sleep(0.1)
 
-send_serial(ser_port, data)
+send_serial(ser_port, frame)
